49271c1cb4b9d8dc858ee8310564636689b6a26e
SorenAndersen1 <andesore@oregonstate.edu>
2020-02-22 12:59:36 -0800

    Basic Data Types

--- Bananaconda.hs
+++ Bananaconda.hs
@@ -1,1 +1,13 @@
 module Bananaconda where
+
+
+
+data Stack = PushS String
+         | PushB Bool
+         | Add
+         | Drop
+         | Equ
+         | IfElse Prog Prog
+         | Size_of_stack
+         | Error
+  deriving (Eq,Show)
