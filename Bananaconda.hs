796636805a7fbac21713f1cff1cf21c13108402d
SorenAndersen1 <andesore@oregonstate.edu>
2020-02-24 17:39:40 -0800

    Doesnt even run, the problem is that I am trying to pass the IO verison of a data type to a function that takes the regular verison of the data type. There is no way to fix this. So the obvious fix is to fix the function to take the IO, but it takes in the value of the stack, which is a custom data type, which you can not make an IO of.

--- Bananaconda.hs
+++ Bananaconda.hs
@@ -1,13 +1,51 @@
 module Bananaconda where
 
-type Stack = [Cmd]
+import System.Random
+
+
+type Load = Stack -> Maybe Stack
+type Stack = [Either Stack_Bools_Ints String]
+type Stack_Bools_Ints = Either Bool Int
+ 
 
 data Cmd = PushS String -- Grace
          | PushB Bool -- Grace
          | Add -- Soren
          | Drop -- Grace
          | Equ -- Grace
-         | IfElse Prog Prog -- Brian
+         | IfElse Stack Stack -- Brian
          | Size_of_stack -- Reed
          | Error -- Brian
+         | Randverb
   deriving (Eq,Show)
+verblist = ["chase", "question", "reach", "kick", "yell"]
+--
+
+
+
+
+cmd :: Cmd -> Load
+cmd Add         = \x -> case x of
+                           (Right i : Right j : x') -> Just (Right (j ++ " " ++ i ) : x')
+                           _ -> Nothing    
+
+cmd Randverb    = \x -> case x of
+                           (Right i : Right j : x') -> Just (Right (randverb verblist) : x')
+                           _ -> Nothing 
+
+--make_IO :: [String] -> IO [String]
+
+
+
+randverbsec :: Int -> String
+randverbsec _ = "Work"
+
+
+randverb ::  [a] -> IO a
+randverb q = do
+         z <- randomRIO (1, 15) :: IO Int
+         return (q !! z)
+--randverb IO 2 = "chase"
+
+
+
