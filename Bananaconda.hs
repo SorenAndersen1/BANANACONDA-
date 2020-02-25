39d605a9a2034f214f6753b859a845cbc661fb4e
Grace Bullock <gracembullock@gmail.com>
2020-02-25 13:13:15 -0800

    Finished my part but commented out reed's thing

--- Bananaconda.hs
+++ Bananaconda.hs
@@ -1,51 +1,44 @@
 module Bananaconda where
 
-import System.Random
 
 
 type Load = Stack -> Maybe Stack
-type Stack = [Either Stack_Bools_Ints String]
-type Stack_Bools_Ints = Either Bool Int
- 
+type Stack = [Either Int String]
+
+
 
 data Cmd = PushS String -- Grace
-         | PushB Bool -- Grace
+         | PushI Int -- Grace
          | Add -- Soren
-         | Drop -- Grace
          | Equ -- Grace
          | IfElse Stack Stack -- Brian
          | Size_of_stack -- Reed
          | Error -- Brian
-         | Randverb
+         | Randverb Int
   deriving (Eq,Show)
 verblist = ["chase", "question", "reach", "kick", "yell"]
---
+nounlist = ["car", "fire extinguisher", "ball", "pool", "tree", "house", "dog", "snake", "computer", "phone", "road", "light", "cave", "baby", "camper"]
 
-
-
+-- Size_of_stack :: Stack -> Int
+-- Size_of_stack []      = 0
+-- Size_of_stack (x:xs)  = 1 + Size_of_stack xs
 
 cmd :: Cmd -> Load
 cmd Add         = \x -> case x of
                            (Right i : Right j : x') -> Just (Right (j ++ " " ++ i ) : x')
-                           _ -> Nothing    
+                           _ -> Nothing
 
-cmd Randverb    = \x -> case x of
-                           (Right i : Right j : x') -> Just (Right (randverb verblist) : x')
-                           _ -> Nothing 
+cmd (Randverb y)   = \x -> case x of
+                           (Right i : Right j : x') -> Just (Right (randverb verblist y) : x')
+                           _ -> Nothing
+cmd (PushS s) = \x -> Just (Right s : x)
+cmd (PushI i) = \x -> Just (Left i : x)
+-- cmd (PushI i) = \x -> Just (Left (_, i) : x)
 
---make_IO :: [String] -> IO [String]
+randverb :: [String] -> Int -> String
+randverb [] _ = " "
+randverb x y = x !! y
 
-
-
-randverbsec :: Int -> String
-randverbsec _ = "Work"
-
-
-randverb ::  [a] -> IO a
-randverb q = do
-         z <- randomRIO (1, 15) :: IO Int
-         return (q !! z)
---randverb IO 2 = "chase"
-
-
-
+drop :: Stack -> Stack
+drop [] = [Left 0] --might have to change to error (underflow)
+drop (x : stack) = stack
