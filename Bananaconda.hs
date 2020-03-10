f5578087d961bd1eb3b4d75f6e6a9a91a996be37
Grace Bullock <gracembullock@gmail.com>
2020-03-10 14:49:59 -0700

    Added palindrome function

--- Bananaconda.hs
+++ Bananaconda.hs
@@ -1,28 +1,21 @@
 module Bananaconda where
+import Prelude hiding (Drop)
 
 
-
+   
 
 type Prog = [Cmd]
 
 
 type Load = Stack -> Maybe Stack
 
-
 type Stack = [Either Int String]
-
-
-
-
-
-
 
 data Cmd = PushS String -- Grace
          | PushI Int -- Grace
          | Add -- Soren
          | Drop -- Grace
          | Equ -- Grace
-
          | IfElse Prog Prog -- Brian
          | Size_of_stack -- Reed
          | Error -- Brian
@@ -30,6 +23,10 @@ data Cmd = PushS String -- Grace
          | Randnoun Int
          | Randadj  Int
   deriving (Eq,Show)
+  
+ex2 :: Prog
+ex2 = [Randadj 2, Randnoun 4, Add]
+
 verblist = ["chase", "question", "reach", "kick", "yell"]
 nounlist = ["car", "fire extinguisher", "ball", "pool", "tree", "house", "dog", "snake", "computer", "phone", "road", "light", "cave", "baby", "camper"]
 adjectivelist = ["jumpy", "slimy", "moist", "cold", "hot", "bright", "hairy", "sticky", "loud", "colorful", "comfy", "soft", "hard", "lumpy", "long"]
@@ -42,28 +39,25 @@ size_of_stack (x:xs)  = 1 + size_of_stack xs
 cmd :: Cmd -> Load
 cmd Add         = \x -> case x of
                            (Right i : Right j : x') -> Just (Right (j ++ " " ++ i ) : x')
+                           (Left i : Left j : x') -> Just (Left (j + i ) : x')
                            _ -> Nothing
 
 cmd (PushS s) = \x -> Just (Right s : x)
 cmd (PushI i) = \x -> Just (Left i : x)
 
-cmd (Randverb y)   = \x -> case x of
-                           (Right i : Right j : x') -> Just (Right (randword verblist y) : x')
-                           _ -> Nothing
+
+cmd (Randverb y)   =  \x -> Just (Right  (randword verblist y) : x)
                            
-cmd (Randnoun y)   = \x -> case x of
-                           (Right i : Right j : x') -> Just (Right (j ++ " " ++ (randword nounlist y) ++ " " ++ i) : x')
-                           _ -> Nothing
+cmd (Randnoun y)   = \x ->  Just (Right  (randword nounlist y) : x)
                            
-cmd (Randadj y)   = \x -> case x of
-                           (Right i : Right j : x') -> Just (Right (j ++ " " ++ (randword adjectivelist y) ++ " " ++ i) : x')
-                           _ -> Nothing
+cmd (Randadj y)   = \x ->  Just (Right  (randword adjectivelist y) : x)
                            
 cmd (IfElse s ss) = \x -> case x of
                         (Left 1 : x') -> prog s x'   --true
                         (Left 0 : x') -> prog ss x'  --false
                         _ -> Nothing
                         
+
 
 
 randword :: [String] -> Int -> String
@@ -76,7 +70,17 @@ prog (c:p)    = \s -> case cmd c s of
                     Just s' -> prog p s'    --if cmd c s succeeds it returns a Just s', -> recursive call to rest of stack
                     _ -> Nothing
 
-drop :: Stack -> Stack
-drop [] = [Left 0] --might have to change to error (underflow)
-drop (x : stack) = stack
+drop_stack :: Stack -> Stack
+drop_stack [] = [Left 0] --might have to change to error (underflow)
+drop_stack (x : stack) = stack
+
+run :: Prog -> Maybe Stack
+run p = prog p []
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = f [] xs xs
+  where
+    f ss xs []              = ss == xs
+    f ss (_:xs) [_]         = ss == xs
+    f ss (x:xs) (_:_:es)    = f (x:ss) xs es
 
