0be6614a0ae03cea8b5709b1185d0477eebc6f56
Brian Kim <kimbri@oregonstate.edu>
2020-03-10 15:17:38 -0700

    yes -ahah

--- Bananaconda.hs
+++ Bananaconda.hs
@@ -1,9 +1,6 @@
 module Bananaconda where
 import Prelude hiding (Drop)
 
-
-
-
 type Prog = [Cmd]
 
 
@@ -12,20 +9,23 @@ type Load = Stack -> Maybe Stack
 type Stack = [Either Int String]
 
 data Cmd = PushS String -- Grace
-         | PushI Int -- Grace
-         | Add -- Soren
-         | Drop -- Grace
-         | Equ -- Grace
-         | IfElse Prog Prog -- Brian
-         | Size_of_stack -- Reed
-         | Error -- Brian
-         | Randverb Int
-         | Randnoun Int
-         | Randadj  Int
+       | PushI Int -- Grace
+       | Add -- Soren
+       | Drop -- Grace
+       | Equ -- Grace
+       | IfElse Prog Prog -- Brian
+       | Size_of_stack -- Reed
+       | Error -- Brian
+       | Randverb Int
+       | Randnoun Int
+       | Randadj  Int
   deriving (Eq,Show)
 
-data Type = TBool | TInt | TError | TString
+data Type = TBool | TInt | TError | TString | TProg
   deriving (Eq, Show)
+
+ex2 :: Prog
+ex2 = [Randadj 2, Randnoun 4, Add]
 
 
 verblist = ["chase", "question", "reach", "kick", "yell"]
@@ -39,9 +39,9 @@ size_of_stack (x:xs)  = 1 + size_of_stack xs
 
 cmd :: Cmd -> Load
 cmd Add         = \x -> case x of
-                           (Right i : Right j : x') -> Just (Right (j ++ " " ++ i ) : x')
-                           (Left i : Left j : x') -> Just (Left (j + i ) : x')
-                           _ -> Nothing
+                         (Right i : Right j : x') -> Just (Right (j ++ " " ++ i ) : x')
+                         (Left i : Left j : x') -> Just (Left (j + i ) : x')
+                         _ -> Nothing
 
 cmd (PushS s) = \x -> Just (Right s : x)
 cmd (PushI i) = \x -> Just (Left i : x)
@@ -54,31 +54,25 @@ cmd (Randnoun y)   = \x ->  Just (Right  (randword nounlist y) : x)
 cmd (Randadj y)   = \x ->  Just (Right  (randword adjectivelist y) : x)
 
 cmd (IfElse s ss) = \x -> case x of
-                        (Left 1 : x') -> prog s x'   --true
-                        (Left 0 : x') -> prog ss x'  --false
-                        _ -> Nothing
+                      (Left 1 : x') -> prog s x'   --true
+                      (Left 0 : x') -> prog ss x'  --false
+                      _ -> Nothing
+
 
 -- Typing Relation
-typeOf :: cmd -> Type
+typeOf :: Cmd -> Type
 typeOf (PushS s)     = TString
 typeOf (PushI i)     = TInt
-typeOf (Add s)       = case (typeOf s) of
-                        (TString)  -> TString
-                        _          -> TError
-typeOf (Randverb y)  = case (typeOf y) of
-                        (TString)  -> TString
-                        _           -> TError
-typeOf (Randnoun y)  = case (typeOf y) of
-                        (TString)  -> TString
-                        _           -> TError
-typeOf (Randadj y)  = case (typeOf y) of
-                        (TString)  -> TString
-                        _           -> TError
-typeOf (IfElse s ss) = case (typeOf s, typeOf ss) of
-                        (ts, tss) -> if ts == tss then ts else TError
-                        _         -> TError
+typeOf Add           = TString
+typeOf (Randverb y)  = TInt
+typeOf (Randnoun y)  = TInt
+typeOf (Randadj y)   = TInt
+typeOf (IfElse s ss) = case (typeOf1 s, typeOf1 ss) of
+                      (ts, tss) -> if ts == tss then ts else TError
+  
 
-
+typeOf1 :: [Cmd] -> Type
+typeOf1 _        = TProg
 
 
 randword :: [String] -> Int -> String
@@ -88,8 +82,8 @@ randword x y = x !! y
 prog :: Prog -> Load
 prog []       = \s -> Just s               -- when empty stack return stack
 prog (c:p)    = \s -> case cmd c s of
-                    Just s' -> prog p s'    --if cmd c s succeeds it returns a Just s', -> recursive call to rest of stack
-                    _ -> Nothing
+                  Just s' -> prog p s'    --if cmd c s succeeds it returns a Just s', -> recursive call to rest of stack
+                  _ -> Nothing
 
 drop_stack :: Stack -> Stack
 drop_stack [] = [Left 0] --might have to change to error (underflow)
@@ -98,5 +92,5 @@ drop_stack (x : stack) = stack
 run :: Prog -> Maybe Stack
 run p = prog p []
 
-ex2 = [PushI 0, IfElse [PushS "5", PushS "6", Add] [PushS "11"]]
-ex3 = [Left 1, Left 2]
+ex4 = [PushI 0, IfElse [PushS "5", PushS "6", Add] [PushS "11"]]
+ex5 = [Left 1, Left 2]
